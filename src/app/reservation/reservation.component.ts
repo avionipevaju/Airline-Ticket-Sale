698bb8b87cc39ed7dec5018a36e6cbeb706fb7f9
avionipevaju <antic.nikola@outlook.com>
2018-02-03 02:33:05 +0100

    Additional ticket booking implementation

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ReservationService} from "./reservation.service";
+import {ReservationService} from './reservation.service';
 declare var $: any;
 
 @Component({
@@ -9,13 +9,10 @@ declare var $: any;
 })
 export class ReservationComponent implements OnInit {
 
-  destinations;
-
   constructor(private reservationService: ReservationService) { }
 
   ngOnInit() {
     this.reservationService.getDestinations();
-    this.destinations = this.reservationService.destinations;
   }
 
   toggleFrom(destination) {
@@ -24,6 +21,17 @@ export class ReservationComponent implements OnInit {
 
   toggleTo(destination) {
     $('#toDropdown').html(destination + ' <span class=\"caret\"></span>');
+  }
+
+  listFlights() {
+    const fromDropdown = document.getElementById('fromDropdown') as HTMLInputElement;
+    const toDropdown = document.getElementById('toDropdown') as HTMLInputElement;
+    this.reservationService.getAllFlights(fromDropdown.innerText, toDropdown.innerText);
+    $('.panel').attr('style', 'visibility: visible');
+  }
+
+  chooseFlight(flightNumber, departureTime) {
+    this.reservationService.getFlightDetails(flightNumber, departureTime);
   }
 
 }
--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 
 @Injectable()
 export class ReservationService {
 
   destinations;
+  flights;
+  details;
   constructor(private http: HttpClient) { }
 
   getDestinations() {
     this.http.get('http://localhost:8080/destinations').subscribe(data => {
       this.destinations = data;
+    });
+  }
+
+  getAllFlights(start, destination) {
+    let params = new HttpParams();
+    params = params.append('start', start);
+    params = params.append('destination', destination);
+    this.http.get('http://localhost:8080/flights/airport', {params: params}).subscribe(data => {
+      this.flights = data;
+    });
+  }
+
+  getFlightDetails(number, departureTime) {
+    let params = new HttpParams();
+    params = params.append('number', number);
+    params = params.append('departureTime', departureTime);
+    this.http.get('http://localhost:8080/flights/details', {params: params}).subscribe(data => {
+      this.details = data;
     });
   }
 
